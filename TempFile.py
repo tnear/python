14b4fdb22fac4e60483bdaf28358ef6623305ad9
tnear <11488488+tnear@users.noreply.github.com>
2025-01-07 13:46:08 -0600

    TempFIle (NamedTemporaryFile)

--- TempFile.py
+++ TempFile.py
@@ -27,11 +27,25 @@ def temp_dir_context_manager():
     with tempfile.TemporaryDirectory() as temp_dir:
         print(f'{temp_dir=}')
 
+# NamedTemporaryFile creates a uniquely named temporary file with parameters
+# for prefix and suffix. The file is deleted when it scope ends.
+def named_temporary_file():
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.dat', prefix='my_file_') as f:
+        # get file name, ex: '/tmp/my_file_<rand>.dat'
+        print(f.name)
+        
+        # write to file
+        f.write('hello')
+        f.flush()
+
+        # file is deleted here
+
 def main():
     get_temp_dir()
     temp_dir()
     make_temp_dir()
     temp_dir_context_manager()
+    named_temporary_file()
 
 if __name__ == '__main__':
     main()
